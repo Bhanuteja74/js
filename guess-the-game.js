084794c254b9e5c3e83de0c599486ef050d75a91
Bhanu Teja Kommi <145745009+Bhanuteja74@users.noreply.github.com>
2024-11-20 11:01:08 +0530

    Update guess-the-game.js

--- guess-the-game.js
+++ guess-the-game.js
@@ -1,31 +1,57 @@
 function getChances(mode) {
-  const chances = mode === 1 ? 4 : 6;
-  return mode === 2 ? chances : 8;
+  if (mode === 1) {
+    return 8;
+  }
+  return mode === 2 ? 6 : 4;
 }
 
-function displayPrompt(gussedNumber) {
-  if (gussedNumber < num) {
-    return 'Your Number is LESSERTHAN the NUMBER';
+function displayPrompt(numToGuess, gussedNumber) {
+  if (gussedNumber < numToGuess) {
+    return 'Your Number is  LESSERTHAN the NUMBER';
   }
   return 'Your Number is GREATERTHAN the NUMBER';
 }
 
-console.log('\n🙏 Welcome To The Game 🙏 --> GUESS THE NUMBER');
-console.log('👉 Number is between 1 and 100.');
-console.log('\nEasy Mode(8)  ->1\nMedian Mode(6) ->2\nHard Mode(4)   ->3')
 
-const mode = +prompt('Select Mode Number:');
-const chances = getChances(mode);
-const num = Math.floor(Math.random() * 100);
+function playGame() {
+  console.log('\nEasy Mode   ->1\nMedian Mode ->2\nHard Mode   ->3');
+  const mode = +prompt('Select Mode Number:');
+  const chances = getChances(mode);
+  const numToGuess = Math.floor(Math.random() * 100);
+  console.log('Yeah you have', chances, 'chances.');
 
-for (let chanceCount = 0; chanceCount < chances; chanceCount++) {
-  const gussedNumber = +prompt('Guess the Number:');
+  for (let chanceCount = 0; chanceCount < chances; chanceCount++) {
+    const gussedNumber = +prompt('Guess the Number:');
+    const isGuessed = gussedNumber === numToGuess;
 
-  if (gussedNumber === num) {
-    console.log('Hurryyyyy 👏👏🎉🎉🎉 YOU GUESSED the number ');
-    break;
+    if (isGuessed) {
+      console.log('Hurryyyyy 👏👏🎉🎉🎉 YOU GUESSED the number ');
+      break;
+    }
+    console.log(displayPrompt(numToGuess, gussedNumber));
   }
-  console.log(displayPrompt(gussedNumber));
+
+  console.log('The Number is:', numToGuess);
 }
 
-console.log('The Number is:', num);
+function readyGame() {
+  playGame();
+
+  if (confirm('You want to Play Again')) {
+    readyGame();
+  }
+}
+
+function startGame() {
+  console.log('\n🙏 Welcome To The Game 🙏 --> GUESS THE NUMBER');
+  console.log('👉 Number is between 1 to 100.');
+  const isReady = confirm('Are you Ready');
+
+  if (isReady) {
+    readyGame();
+  }
+
+  console.log('Thank You for Visiting Game.\n   Have a 🥚🥚🥚 DAY.');
+}
+
+startGame();
