8ddb7c3c53a92534442d4f00368c32465049f6cf
Bhanu Teja Kommi <145745009+Bhanuteja74@users.noreply.github.com>
2024-11-27 10:07:46 +0530

    Update car.js

--- car.js
+++ car.js
@@ -1,15 +1,55 @@
 const WIDTH = 100;
-const HEIGHT = 4;
+const HEIGHT = 5;
 
-function carParts(index) {
-  switch (index) {
-    case 0: return '  _____         ';//carTop
-    case 1: return ' /|_||_`.__     ';//carDoor
-    case 2: return '(   _    _ _\\   ';//carSide
-    case 3: return ' - (_)--(_) -\'  ';//carBottom
-    default: return '                ';//
+const bridge = [
+  '          \'x|`                     ',
+  '        \'|xx| `          \'|x|.     ',
+  '`   \'    |xx|    `   \'    |x|`     ',
+  '============|===============|===-- ',
+  '    ~~~~~|xx|~~~~~~~~~~~~~|x|~~~ ~~',
+  '                                   '
+];     // length is 35
+
+// function bridge(index) {
+//   switch (index) {
+//     case 0: return '          \'x|`                     ';
+//     case 1: return '        \'|xx| `          \'|x|.     ';
+//     case 2: return '`   \'    |xx|    `   \'    |x|`     ';
+//     case 3: return '============|===============|===-- ';
+//     case 4: return '    ~~~~~|xx|~~~~~~~~~~~~~|x|~~~ ~~';
+//     default: return '                                   ';
+//   }
+// }
+
+function putObject(screen, width, heigth, object, objectLength) {
+  let updatedScreen = '';
+
+  for (let index = 0; index < heigth; index++) {
+    const start = (index * width) + objectLength;
+    const end = (index + 1) * width;
+    updatedScreen += object[index] + slice(screen, start, end);
   }
+
+  return updatedScreen;
 }
+
+const carParts = [
+  '  _____         ',
+  ' /|_||_`.__     ',
+  '(   _    _ _\\   ',
+  ' - (_)--(_) -\'  ',
+  '                '
+]; // length is 16
+
+// function carParts(index) {
+//   switch (index) {
+//     case 0: return '  _____         ';//carTop
+//     case 1: return ' /|_||_`.__     ';//carDoor
+//     case 2: return '(   _    _ _\\   ';//carSide
+//     case 3: return ' - (_)--(_) -\'  ';//carBottom
+//     default: return ' - (_)--(_) -\'  ';//
+//   }
+// }
 
 function wait(seconds) {
   for (let s = 0; s < seconds * 100; s++) {
@@ -54,6 +94,7 @@ function movedScreen(screen, speed, end) {
   return frontScreen;
 }
 
+// moving left to right
 function moveObject(screen, width, heigth, speed) {
   let movedObject = '';
   for (let y = 0; y < heigth; y++) {
@@ -65,24 +106,27 @@ function moveObject(screen, width, heigth, speed) {
   return movedObject;
 }
 
-function putScreen(screen, width, heigth) {
+function displayScreen(screen, width, heigth) {
   for (let y = 0; y < heigth; y++) {
     const x = y * width;
     console.log(slice(screen, x, x + width));
   }
 }
 
-function carMove() {
-  let screen = repeat('-', WIDTH * HEIGHT);
-  screen = putCar(screen, WIDTH, HEIGHT, 16);
-  putScreen(screen, WIDTH, HEIGHT);
+function objectMotion() {
+  let screen = repeat(' ', WIDTH * HEIGHT);
+  let carScreen = putObject(screen, WIDTH, HEIGHT, carParts, 16);
+  let bridgeScreen = putObject(screen, WIDTH, HEIGHT, bridge, 35);
 
-  for (let i = 0; i < 100; i++) {
+  for (let i = 0; i < 50; i++) {
     console.clear();
-    screen = moveObject(screen, WIDTH, HEIGHT, 1);
-    putScreen(screen, WIDTH, HEIGHT);
+    // bridgeScreen = moveObject(bridgeScreen, WIDTH, HEIGHT, 1);
+    displayScreen(bridgeScreen, WIDTH, HEIGHT);
+
+    carScreen = moveObject(carScreen, WIDTH, HEIGHT, 1);
+    displayScreen(carScreen, WIDTH, HEIGHT);
     wait(.7);
   }
 }
 
-carMove()
+objectMotion()
